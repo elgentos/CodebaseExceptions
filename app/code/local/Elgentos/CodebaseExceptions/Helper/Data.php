4ae3f94eadee1cea47bb35579d2b8c9baca7627d
Peter Jaap Blaakmeer <info@blaakmeer.com>
2012-08-22 15:44:09 +0200

    Cleaned up helper

--- app/code/local/Elgentos/CodebaseExceptions/Helper/Data.php
+++ app/code/local/Elgentos/CodebaseExceptions/Helper/Data.php
@@ -8,7 +8,7 @@ class Elgentos_CodebaseExceptions_Helper_Data extends Mage_Core_Helper_Abstract 
         require_once Mage::getBaseDir('lib') . '/Airbrake/Client.php';
         require_once Mage::getBaseDir('lib') . '/Airbrake/Configuration.php';
 
-        $apiKey  = '0e806c08-1ce1-2a5f-672a-92509a8a9f81'; // This is required
+        $apiKey  = Mage::getStoreConfig('codebaseexceptions/general/apikey');
         $options = array();
         $requestUri = explode("/",$_SERVER['REQUEST_URI']);
         $options['action'] = array_pop($requestUri);
@@ -16,15 +16,6 @@ class Elgentos_CodebaseExceptions_Helper_Data extends Mage_Core_Helper_Abstract 
         $projectRoot = explode('/',$_SERVER['PHP_SELF']);
         array_pop($projectRoot);
         $options['projectRoot'] = implode('/',$projectRoot).'/';
-        if(stripos($_SERVER['HTTP_HOST'],'dev.')!==false) {
-            $options['environmentName'] = 'development';
-            $devs = array('peterjaap','janhenk','wouter');
-            foreach($devs as $dev) {
-                if(substr($_SERVER['REQUEST_URI'],1,strlen($dev))==$dev) {
-                    $options['environmentName'] .= ' - ' . $dev;
-                }
-            }
-        }
         $config = new Airbrake\Configuration($apiKey,$options);
         $this->client = new Airbrake\Client($config);
     }
